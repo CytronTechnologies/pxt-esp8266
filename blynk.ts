a8446b0902f71715dc8a4bc949834421b93bf8d6
Thefahmialias <fauzanfahmi89@gmail.com>
2024-08-08 13:52:12 +0800

    Update blynk.ts

--- blynk.ts
+++ blynk.ts
@@ -14,7 +14,8 @@ namespace esp8266 {
 
     // Blynk servers.
     let blynkServers = ["blynk.cloud", "fra1.blynk.cloud", "lon1.blynk.cloud",
-                        "ny3.blynk.cloud", "sgp1.blynk.cloud", "blr1.blynk.cloud"]
+                        "ny3.blynk.cloud", "sgp1.blynk.cloud", "blr1.blynk.cloud"
+                        "iot.serangkota.go.id"]
 
 
 
